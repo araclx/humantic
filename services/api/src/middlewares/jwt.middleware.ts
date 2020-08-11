8c1eea2d93582e6c5f8b712db90693dda6bc76b6
Jakub Olan <jakub.olan001@gmail.com>
2020-08-11 15:32:43 +0200

    feat(auth): add jwt middleware

---
+++ services/api/src/middlewares/jwt.middleware.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import jwt from 'express-jwt'
+import jwksrsa from 'jwks-rsa'
+
+import { AUTH0_DOMAIN, AUTH0_AUDIENCE } from '../utils/env'
+
+export const JWT = jwt({
+	secret: jwksrsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`,
+	}),
+
+	audience: AUTH0_AUDIENCE,
+	issuer: `https://${AUTH0_DOMAIN}`,
+	algorithms: ['RS256'],
+})
--- services/api/src/utils/env.ts
+++ services/api/src/utils/env.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const MONGODB_URI: string = process.env.MONGODB_URI! || 'mongodb://localhost'
+const AUTH0_AUDIENCE: string = process.env.AUTH0_AUDIENCE!
+const AUTH0_DOMAIN: string = process.env.AUTH0_DOMAIN!
 const SENTRY_DSN: string = process.env.SENTRY_DSN!
 const ENVPORT: number = Number.parseInt(process.env.HUMANTIC_PORT!) || 3600
 
-export { MONGODB_URI, SENTRY_DSN, ENVPORT }
+export { MONGODB_URI, SENTRY_DSN, ENVPORT, AUTH0_AUDIENCE, AUTH0_DOMAIN }
