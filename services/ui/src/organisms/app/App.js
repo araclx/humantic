aa8e1e6b1450fa1b78bed6f9be06ffafbdf88375
Jakub Olan <jakub.olan001@gmail.com>
2020-08-13 06:35:15 +0200

    refactor: update files

--- services/ui/src/organisms/app/App.js
+++ services/ui/src/organisms/app/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import LoginButton from '../../molecules/login.molecule'
 import LogoutButton from '../../molecules/logout.molecule'
@@ -9,7 +8,6 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
