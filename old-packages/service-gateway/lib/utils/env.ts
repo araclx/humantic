f0aca7cd106d03fc41e4440dd24de14b2eb4a056
Jakub Olan <jakub.olan001@gmail.com>
2020-08-19 12:06:00 +0200

    refactor: remove unused packages

--- old-packages/service-gateway/lib/utils/env.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-// eslint-disable-next-line radix
-export const APP_PORT = Number.parseInt(process.env.HUMANTIC_GATEWAY_PORT!) || 3600
--- old-packages/service-project/lib/project.reponder.ts
+++
@@ -1,8 +0,0 @@
-import cote from 'cote'
-
-const reponder = new cote.Responder({
-	name: 'project responder',
-	key: 'projects',
-})
-
-export default reponder
