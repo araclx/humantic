f0de2fe4489150d952207c45037eca26f36f48b9
Jakub Olan <jakub.olan001@gmail.com>
2020-08-19 12:00:37 +0200

    feat: add jwt middleware

--- packages/service-api/src/utils/env.ts
+++ packages/service-api/src/utils/env.ts
@@ -5,3 +5,5 @@ dotenv.config()
 export const HOST = process.env.HOST! || 'localhost'
 // eslint-disable-next-line radix
 export const PORT = Number.parseInt(process.env.HUMANTIC_PORT!) || 3600
+export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN!
+export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE!
