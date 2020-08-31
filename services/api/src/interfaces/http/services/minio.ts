862543bad91f4c2fe3f0d04df83c3cde456cbdb1
Jakub Olan <jakub.olan001@gmail.com>
2020-08-31 23:06:37 +0200

    refactor: update variable name in minioService

--- services/api/src/interfaces/http/services/minio.ts
+++ services/api/src/interfaces/http/services/minio.ts
@@ -2,7 +2,7 @@ import * as Minio from 'minio'
 import signale from 'signale'
 import { MINIO_HOST, MINIO_PRIVATEKEY, MINIO_PUBLICKEY, NODE_ENV } from 'utils/env'
 
-const minioClient = new Minio.Client({
+const minio = new Minio.Client({
 	endPoint: MINIO_HOST,
 	port: 9000,
 	useSSL: false,
@@ -16,13 +16,13 @@ if (NODE_ENV === 'CI') logger.disable()
 
 export function minioService() {
 	// Check actual instance of humantic-projects, and if there is no such bucket create a new one.
-	minioClient.bucketExists('humantic-projects', function (err, exist) {
+	minio.bucketExists('humantic-projects', function (err, exist) {
 		if (err) logger.error(err)
 
 		if (exist) return logger.warn('bucket "humantic-projects" already exitsts.')
 
 		if (!exist) {
-			minioClient.makeBucket('humantic-projects', 'eu', function (err) {
+			minio.makeBucket('humantic-projects', 'eu', function (err) {
 				if (err) return logger.error(err)
 				logger.success('successfully created bucket "humantic-projects"')
 			})
@@ -30,7 +30,7 @@ export function minioService() {
 	})
 
 	// List all available buckets.
-	minioClient.listBuckets(function (err, buckets) {
+	minio.listBuckets(function (err, buckets) {
 		if (err) return logger.error(err)
 		logger.info('listing available buckets...')
 		logger.log(buckets)
